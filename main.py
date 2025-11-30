66d4e1489cd071088b84443a03810e752d7caae2
Seth Gallucci <sethgallucci@yahoo.com>
2025-11-30 02:59:05 -0500

    Add stock fetching with llm recommendation This commit adds the following workflow to the app: - connect to llm - sample random cluster profile (placeholder for proper mapping) - prompt llm to recommend ticker symbols - fetch yFinance stock data from tickers - display each stock's information with 1y price history plot

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import streamlit as st
-import time
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage
+from dataclasses import dataclass
+import yfinance as yf
+from pandas import DataFrame
+from plotly import express as px
+from random import sample
+from json import load as load_json
+
 
 ################
 # Initial load
 ################
+st.set_page_config(layout="wide")
+
 if "stage" not in st.session_state:
     st.session_state["stage"] = "survey"
 
@@ -16,10 +26,11 @@ def reset_session_state():
     st.session_state["stage"] = "survey"
     st.session_state["user_profile"] = {}
 
+
 ################
 # Survey stage
 ################
-def make_survey_question(question_name, answers):
+def make_survey_question(question_name, answers) -> st.radio:
     return st.radio(
         question_name,
         answers,
@@ -60,37 +71,126 @@ def render_survey():
                 "q3": q3,
             }
 
-            # HARD-CODED UNTIL SURVEY CLUSTER MAPPING IS SET UP.
-            st.session_state["cluster_result"] =\
-                "Frugal, low-profile, and cautious."
-
             st.session_state["stage"] = "recommending"
             st.rerun()
+
 
 ################
 # Processing recommendations stage
 ################
+@dataclass
+class StockInfo:
+    name: str
+    ticker_symbol: str
+    price: float | None
+    price_to_earnings: float | None
+    history: DataFrame | None
+    description: str
+
+def get_stock_info(tickers: list[str]) -> list[StockInfo]:
+    stocks = []
+    for ticker in tickers:
+        try:
+            stock = yf.Ticker(ticker)
+            stock_name = stock.info.get("shortName", None)
+            if stock_name is None:
+                raise ValueError(f"No stock found for {ticker}.")
+
+            hist = stock.history("1y").rename(columns={"Close": "Price"})
+
+            stocks.append(StockInfo(
+                name = stock_name,
+                ticker_symbol = ticker,
+                price = stock.info.get("currentPrice", None),
+                price_to_earnings = stock.info.get("trailingPE", None),
+                history = hist[["Price"]] if len(hist) > 0 else None,
+                description = stock.info.get(
+                    "longBusinessSummary",
+                    "No description available.",
+                ),
+            ))
+
+        except Exception as e:
+            pass
+
+    if len(stocks) == 0:
+        st.error("Unable to fetch stock data.")
+        st.stop()
+
+    return stocks
+
+
 def render_recommending_phase():
+
+    # CHANGE TO EXTERNAL ENDPOINT AND ENV FILE KEYS WHEN READY.
+    llm = ChatOpenAI(
+        base_url="http://localhost:1234/v1",
+        api_key="LM-Studio",
+        temperature=0.5,
+    )
+
     st.title("Generating recommendations...")
-
     progress_bar = st.progress(0)
-    status_text = st.empty()
 
-    steps = [
-        "Mapping user profile to clusters...",
-        "Identifying suitable tickers...",
-        "Fetching live data from Yahoo Finance...",
-        "Drafting recommendations..."
-    ]
 
-    # MOCK GENERATION STAGES WHILE NOT FULLY IMPLEMENTED
-    for i, step in enumerate(steps):
-        status_text.text(step)
-        progress_bar.progress((i + 1) * 25)
-        time.sleep(0.8)
+    # Step 1 - Determine cluster
+    progress_bar.progress(25, "Mapping responses...")
+
+    with open("clusters.json", "r") as clusters_file:
+        clusters = load_json(clusters_file)
+
+    # REPLACE WITH CLUSTER MAPPING WHEN READY.
+    st.session_state["cluster_result"] = sample(clusters, 1)[0]
+
+    # Step 2 - Get AI recommendation
+    progress_bar.progress(50, "Consulting recommender...")
+
+    cr = st.session_state.get("cluster_result", "Unknown")
+    # We should consider loading this from another file.
+    prompt = f"""
+    You are an AI agent meant to recommend tickers to check out for users based
+    on clustering profile from a short survey.
+    
+    This user's result was:
+    - Profile: {cr["profile"]}
+    - Why: {cr["why"]}
+    - Investment Profile: {cr["investment_profile"]}
+    - Priority: {cr["priority"]}
+    - Examples: {", ".join(cr["examples"])}
+    
+    Recommend exactly 3 unique stock tickers that align with this profile description.
+    Return only the stock tickers separated by a space. Format these for 
+    yFinance. Do not add anything else.
+    
+    Example response: NVDA BRK-B AAPL
+    """
+
+    try:
+        response = llm.invoke([
+            SystemMessage(content=prompt),
+        ])
+    # Errors will be handled specifically if there is time later.
+    except Exception as e:
+        st.error("Unable to connect to AI agent.")
+        st.stop()
+
+
+    # Step 3 - Parse ticker symbols
+    progress_bar.progress(75, "Processing recommendations...")
+
+    st.session_state["tickers"] = list(set(response.content.strip().split()))
+    if len(st.session_state["tickers"]) < 1:
+        st.error("No stock tickers found.")
+
+
+    # Step 4 - Fetch ticker data
+    progress_bar.progress(100, "Fetching market data...")
+
+    st.session_state["stocks"] = get_stock_info(st.session_state["tickers"])
 
     st.session_state["stage"] = "chat"
     st.rerun()
+
 
 ################
 # results and AI chat stage
@@ -98,9 +198,49 @@ def render_recommending_phase():
 def render_agent_chat():
 
     st.title("Your Recommendations")
-    st.info("Tickers and charts will appear here.")
+    st.write(st.session_state["cluster_result"])
 
-    st.subheader("Chat")
+    for stock in st.session_state["stocks"]:
+        st.divider()
+        st.subheader(stock.name)
+
+        ticker_col, price_col, pte_col = st.columns(3)
+        if stock.ticker_symbol is not None:
+            ticker_col.markdown(
+                f"""
+                <div style='text-align: left;'>
+                    Symbol: {stock.ticker_symbol}
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
+        if stock.price is not None:
+            price_col.markdown(
+                f"""
+                <div style='text-align: center;'>
+                    Current Price: {stock.price}
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
+        if stock.price_to_earnings is not None:
+            pte_col.markdown(
+                f"""
+                <div style='text-align: right;'>
+                    Price to Earnings: {stock.price_to_earnings}
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
+
+        if stock.history is not None:
+            st.plotly_chart(px.line(stock.history, y="Price"))
+
+        with st.expander("Business Summary"):
+            st.write(stock.description)
+
+    st.divider()
+    st.subheader("AI Agent")
     st.chat_message("assistant").write(
         "This is placeholder text. In the future, I'll be able to have a full "
         "conversation with you!")
