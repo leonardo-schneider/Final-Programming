212e7dea7c3fdeb86d59ef3ebb6a43283cc838a0
Seth Gallucci <sethgallucci@yahoo.com>
2025-11-29 22:03:36 -0500

    Add Streamlit app skeleton This commit creates a rough outline for the streamlit app with placeholders for - user survey - backend-work processing screen - results and ai chat

--- main.py
+++ main.py
@@ -1,30 +1,116 @@
-#%%
-import kagglehub
-from kagglehub import KaggleDatasetAdapter
-import pandas as pd
-from dotenv import load_dotenv
-import os
+import streamlit as st
+import time
+
+# STAGE - SURVEY
+
+if "stage" not in st.session_state:
+    st.session_state["stage"] = "survey"
+
+if "user_profile" not in st.session_state:
+    st.session_state["user_profile"] = {}
+
+def reset_session_state():
+    st.session_state["stage"] = "survey"
+    st.session_state["user_profile"] = {}
 
 
-load_dotenv()
 
-
-
-try:
-    df = kagglehub.load_dataset(
-        KaggleDatasetAdapter.PANDAS,
-        "miroslavsabo/young-people-survey",
-        "responses.csv", 
+def make_survey_question(question_name, answers):
+    return st.radio(
+        question_name,
+        answers,
+        horizontal=True,
+        width="stretch",
     )
 
+def render_survey():
+    st.title("Survey")
+    st.write("Take this survey, to help us narrow down recommendations.")
 
-    print("\n--- SHAPE ---")
-    print(f"Linhas: {df.shape[0]}")
-    print(f"Colunas: {df.shape[1]}")
+    with st.form("survey_form"):
+        # We should consider loading these from a file.
+        q0 = make_survey_question(
+            "Question 0",
+            [1,2,3,4,5],
+        )
+        q1 = make_survey_question(
+            "Question 1",
+            [1,2,3,4,5],
+        )
+        q2 = make_survey_question(
+            "Question 2",
+            [1,2,3,4,5],
+        )
+        q3 = make_survey_question(
+            "Question 3",
+            [1,2,3,4,5],
+        )
 
-except Exception as e:
-    print(f"\nErro: {e}")
+        submitted_survey = st.form_submit_button("Continue")
 
-#%%
-df
-#%%
+        if submitted_survey:
+            st.session_state["user_profile"] = {
+                "q0": q0,
+                "q1": q1,
+                "q2": q2,
+                "q3": q3,
+            }
+
+            # HARD-CODED UNTIL SURVEY CLUSTER MAPPING IS SET UP.
+            st.session_state["cluster_result"] =\
+                "Frugal, low-profile, and cautious."
+
+            st.session_state["stage"] = "recommending"
+            st.rerun()
+
+
+def render_recommending_phase():
+    st.title("Generating recommendations...")
+
+    progress_bar = st.progress(0)
+    status_text = st.empty()
+
+    steps = [
+        "Mapping user profile to clusters...",
+        "Identifying suitable tickers...",
+        "Fetching live data from Yahoo Finance...",
+        "Drafting recommendations..."
+    ]
+
+    # MOCK GENERATION STAGES WHILE NOT FULLY IMPLEMENTED
+    for i, step in enumerate(steps):
+        status_text.text(step)
+        progress_bar.progress((i + 1) * 25)
+        time.sleep(0.8)
+
+    st.session_state["stage"] = "chat"
+    st.rerun()
+
+
+
+def render_agent_chat():
+
+    st.title("Your Recommendations")
+    st.info("Tickers and charts will appear here.")
+
+    st.subheader("Chat")
+    st.chat_message("assistant").write(
+        "This is placeholder text. In the future, I'll be able to have a full "
+        "conversation with you!")
+
+    if prompt := st.chat_input("Converse with the AI agent..."):
+        st.chat_message("user").write(prompt)
+        st.chat_message("assistant").write("Sorry, but I'm not yet an AI agent!")
+
+    st.button("Start Over", on_click=reset_session_state)
+
+
+
+if st.session_state['stage'] == 'survey':
+    render_survey()
+
+elif st.session_state['stage'] == 'recommending':
+    render_recommending_phase()
+
+elif st.session_state['stage'] == 'chat':
+    render_agent_chat()
